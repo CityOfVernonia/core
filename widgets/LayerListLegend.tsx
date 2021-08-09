f1d26e3ee18dd37744a03701b22e14ccbf02de78
City of Vernonia GIS <gis@vernonia-or.gov>
2021-08-09 07:51:30 -0700

    update layer list legend

--- widgets/LayerListLegend.tsx
+++ widgets/LayerListLegend.tsx
@@ -15,9 +15,10 @@ import LayerList from '@arcgis/core/widgets/LayerList';
 import Legend from '@arcgis/core/widgets/Legend';
 
 // styles
-import './LayerListLegend/styles/LayerListLegend.scss';
+import './LayerListLegend.scss';
 const CSS = {
   base: 'cov-layer-list-legend',
+  scroll: 'cov-layer-list-legend--scroll',
 };
 
 // class export
@@ -26,41 +27,52 @@ export default class LayerListLegend extends Widget {
   @property()
   view!: esri.MapView | esri.SceneView;
 
+  @property()
+  theme = 'light';
+
+  @property()
+  scale = 'm';
+
   constructor(properties: cov.LayerListLegendProperties) {
     super(properties);
   }
 
   render(): tsx.JSX.Element {
+    const { theme, scale } = this;
     return (
       <div class={CSS.base}>
-        <calcite-tabs layout="center">
+        <calcite-tabs them={theme} scale={scale} layout="center">
           <calcite-tab-nav slot="tab-nav">
             <calcite-tab-title active="">Layers</calcite-tab-title>
             <calcite-tab-title>Legend</calcite-tab-title>
           </calcite-tab-nav>
           <calcite-tab active="">
-            <div
-              bind={this}
-              afterCreate={(container: HTMLDivElement) => {
-                const { view } = this;
-                new LayerList({
-                  view,
-                  container,
-                });
-              }}
-            ></div>
+            <div class={CSS.scroll}>
+              <div
+                bind={this}
+                afterCreate={(container: HTMLDivElement) => {
+                  const { view } = this;
+                  new LayerList({
+                    view,
+                    container,
+                  });
+                }}
+              ></div>
+            </div>
           </calcite-tab>
           <calcite-tab>
-            <div
-              bind={this}
-              afterCreate={(container: HTMLDivElement) => {
-                const { view } = this;
-                new Legend({
-                  view,
-                  container,
-                });
-              }}
-            ></div>
+            <div class={CSS.scroll}>
+              <div
+                bind={this}
+                afterCreate={(container: HTMLDivElement) => {
+                  const { view } = this;
+                  new Legend({
+                    view,
+                    container,
+                  });
+                }}
+              ></div>
+            </div>
           </calcite-tab>
         </calcite-tabs>
       </div>
