d0fd9d0628d2660120e42f42fd53099bbcedf4de
City of Vernonia GIS <gis@vernonia-or.gov>
2022-01-19 07:43:54 -0800

    make header user control internal class

--- Application/_widgets.tsx
+++ Application/_widgets.tsx
@@ -2,7 +2,6 @@ import cov = __cov;
 import { property, subclass } from '@arcgis/core/core/accessorSupport/decorators';
 import Widget from '@arcgis/core/widgets/Widget';
 import { tsx } from '@arcgis/core/widgets/support/widget';
-import { UserControl } from './UserControl';
 import ZoomViewModel from '@arcgis/core/widgets/Zoom/ZoomViewModel';
 // TODO: work on dynamic loading that doesn't break widget for optional VMs
 import HomeViewModel from '@arcgis/core/widgets/Home/HomeViewModel';
@@ -15,6 +14,10 @@ const CSS = {
   headerTitle: 'cov-application--header--title',
   headerSearch: 'cov-application--header--search',
   headerUser: 'cov-application--header--user',
+  // user widget
+  user: 'cov-application--user',
+  userInfo: 'cov-application--user--info',
+  userLinks: 'cov-application--user--links',
   // ui widget selector
   selectorPanels: 'cov-application--ui-selector--panels',
   selectorRight: 'cov-application--ui-selector--right',
@@ -238,7 +241,7 @@ export class _Header extends Widget {
     const { oAuthViewModel } = this;
 
     if (oAuthViewModel) {
-      new UserControl({
+      new _User({
         oAuthViewModel,
         container,
       });
@@ -314,3 +317,85 @@ export class _Selector extends Widget {
     );
   }
 }
+
+/**
+ * User control widget for default header.
+ */
+ @subclass('cov.Application._User')
+ class _User extends Widget {
+   constructor(properties: esri.WidgetProperties & { oAuthViewModel: cov.OAuthViewModel }) {
+     super(properties);
+   }
+ 
+   oAuthViewModel!: cov.OAuthViewModel;
+ 
+   render(): esri.widget.tsx.JSX.Element {
+     const { id, oAuthViewModel } = this;
+ 
+     const _id = `popover_${id}`;
+ 
+     return oAuthViewModel.signedIn ? (
+       <div>
+         <calcite-popover-manager auto-close="">
+           <calcite-popover placement="left-leading" overlay-positioning="fixed" reference-element={_id}>
+             <div class={CSS.user}>
+               <div class={CSS.userInfo}>
+                 <calcite-avatar
+                   scale="l"
+                   username={oAuthViewModel.username}
+                   full-name={oAuthViewModel.name}
+                   thumbnail={
+                     oAuthViewModel.user && oAuthViewModel.user.thumbnailUrl ? oAuthViewModel.user.thumbnailUrl : ''
+                   }
+                 ></calcite-avatar>
+                 <div>
+                   <div>{oAuthViewModel.name}</div>
+                   <div>{oAuthViewModel.username}</div>
+                 </div>
+               </div>
+               <div class={CSS.userLinks}>
+                 <calcite-link href={`${oAuthViewModel.portal.url}/home/content.html`} target="_blank">
+                   My Content
+                 </calcite-link>
+                 <calcite-link href={`${oAuthViewModel.portal.url}/home/user.html`} target="_blank">
+                   My Profile
+                 </calcite-link>
+               </div>
+               <calcite-button
+                 width="full"
+                 appearance="outline"
+                 icon-start="sign-out"
+                 onclick={oAuthViewModel.signOut.bind(oAuthViewModel)}
+               >
+                 Sign Out
+               </calcite-button>
+             </div>
+           </calcite-popover>
+           <calcite-avatar
+             id={_id}
+             style="cursor: pointer;"
+             scale="s"
+             username={oAuthViewModel.username}
+             full-name={oAuthViewModel.name}
+             thumbnail={oAuthViewModel.user && oAuthViewModel.user.thumbnailUrl ? oAuthViewModel.user.thumbnailUrl : ''}
+             title={`Signed in as ${oAuthViewModel.name}`}
+           ></calcite-avatar>
+         </calcite-popover-manager>
+       </div>
+     ) : (
+       <div>
+         <calcite-button
+           icon-start="sign-in"
+           scale="s"
+           round=""
+           appearance="transparent"
+           color="inverse"
+           title="Sign In"
+           onclick={oAuthViewModel.signIn.bind(oAuthViewModel)}
+         >
+           Sign In
+         </calcite-button>
+       </div>
+     );
+   }
+ }
