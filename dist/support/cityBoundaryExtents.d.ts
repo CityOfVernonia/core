c0e46fc355d5eda31cf8d2b8eb6a07a1d9d8c43b
City of Vernonia GIS <gis@vernonia-or.gov>
2025-04-09 13:59:00 -0700

    Release 5.0.16

--- dist/support/cityBoundaryExtents.d.ts
+++ dist/support/cityBoundaryExtents.d.ts
@@ -1,11 +1,14 @@
 import esri = __esri;
-/**
- * Return City Limits feature layer and extents for initializing view.
- * @param id City Limits portal item id
- */
-declare const cityBoundaryExtents: (id: string) => Promise<{
+interface I {
     cityLimits: esri.FeatureLayer;
     extent: esri.Extent;
     constraintExtent: esri.Extent;
-}>;
+    geometry: esri.Polygon;
+}
+/**
+ * Return City Limits feature layer and extents for initializing view.
+ * @param id Portal item id
+ * @param geometryOutSpatialReference Spatial reference of City Limits geometry (defaults to 4326)
+ */
+declare const cityBoundaryExtents: (id: string, geometryOutSpatialReference?: esri.SpatialReference | esri.SpatialReferenceProperties) => Promise<I>;
 export default cityBoundaryExtents;
--- src/support/cityBoundaryExtents.ts
+++ src/support/cityBoundaryExtents.ts
@@ -3,17 +3,22 @@
 //////////////////////////////////////
 import esri = __esri;
 
-/**
- * Return City Limits feature layer and extents for initializing view.
- * @param id City Limits portal item id
- */
-const cityBoundaryExtents = async (
-  id: string,
-): Promise<{
+interface I {
   cityLimits: esri.FeatureLayer;
   extent: esri.Extent;
   constraintExtent: esri.Extent;
-}> => {
+  geometry: esri.Polygon;
+}
+
+/**
+ * Return City Limits feature layer and extents for initializing view.
+ * @param id Portal item id
+ * @param geometryOutSpatialReference Spatial reference of City Limits geometry (defaults to 4326)
+ */
+const cityBoundaryExtents = async (
+  id: string,
+  geometryOutSpatialReference?: esri.SpatialReference | esri.SpatialReferenceProperties,
+): Promise<I> => {
   const cityLimits = new (await import('@arcgis/core/layers/FeatureLayer')).default({
     portalItem: {
       id,
@@ -22,13 +27,17 @@ const cityBoundaryExtents = async (
 
   await cityLimits.load();
 
+  const geometry = (
+    await cityLimits.queryFeatures({
+      outSpatialReference: geometryOutSpatialReference || { wkid: 4326 },
+      returnGeometry: true,
+      where: '1 = 1',
+    })
+  ).features[0].geometry as esri.Polygon;
+
   const extent = (cityLimits.fullExtent as esri.Extent).clone();
 
-  return {
-    cityLimits,
-    extent,
-    constraintExtent: extent.clone().expand(3),
-  };
+  return { cityLimits, constraintExtent: extent.clone().expand(3), extent, geometry };
 };
 
 export default cityBoundaryExtents;
